da061ec5fdaa817641667e4a2fda7c8d8bc7524f
ashiq-a-r <ashiqkallara.cet@gmail.com>
2026-06-30 09:45:54 +0530

    chore: lexer to extract tokens

---
+++ src/slang/lexer/Lexer.java
@@ -0,0 +1,4 @@
+package slang.lexer;
+
+public class Lexer {
+}
---
+++ src/slang/lexer/Token.java
@@ -0,0 +1,13 @@
+package slang;
+
+public enum Token {
+    ILLEGAL_TOKEN,
+    TOKEN_PLUS,
+    TOKEN_SUB,
+    TOKEN_MUL,
+    TOKEN_DIV,
+    TOKEN_OPAREN,
+    TOKEN_CPAREN,
+    TOKEN_DOUBLE,
+    TOKEN_NULL
+}
